4913dc35210eadcb15a2251f2c98f499b708f717
drammopo <drammopo@gmail.com>
2014-04-23 12:40:40 -0700

    Completed Medusa exercise

---
+++ mythical-creatures/medusa.rb
@@ -0,0 +1,37 @@
+class Medusa
+	attr_reader :name
+
+	def initialize(name)
+		@name = name		
+		@statues = []
+	end
+
+	def stare(victim)
+		victim.stoned = true
+		@statues << victim
+	end
+
+	def statues
+		@statues
+	end
+end
+
+class Person
+
+	def initialize(name)
+		@name = name
+		@stoned = false		
+	end
+
+	def name
+		@name
+	end
+
+	def stoned?
+		@stoned
+	end
+
+	def stoned=(new_stoned)
+		@stoned = new_stoned
+	end
+end
