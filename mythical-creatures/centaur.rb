73c771cbb7b29e11785b850e4c28e9bff8a144e5
drammopo <drammopo@gmail.com>
2014-04-24 00:43:28 -0700

    Completed Centaur exercise

---
+++ mythical-creatures/centaur.rb
@@ -0,0 +1,47 @@
+class Centaur
+	attr_reader :name, :breed
+
+	def initialize(name, breed)
+		@name = name
+		@breed = breed	
+		@mood = 0
+		@standing = true
+	end
+
+	def shoot
+		return "NO!" if laying?
+		@mood -= 1
+		cranky? ? "NO!": "Twang!!!"
+	end
+
+	def run
+		return "NO!" if laying?
+		@mood -= 1
+		cranky? ? "NO!": "Clop clop clop clop!!!"
+	end
+
+	def cranky?
+		@mood <= -3
+	end
+
+	def lay_down
+		@standing = false
+	end
+
+	def laying?
+		@standing == false
+	end
+
+	def stand_up
+		@standing = true
+	end
+
+	def standing?
+		@standing == true
+	end
+
+	def sleep
+		return "NO!" if standing?
+		@mood = 0
+	end
+end
