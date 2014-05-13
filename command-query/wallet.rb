98c3923a6ce1831593b2d2cac4e019152e5dcca1
drammopo <drammopo@gmail.com>
2014-05-13 20:49:21 +0200

    Refactored code

--- command-query/wallet.rb
+++ command-query/wallet.rb
@@ -1,26 +1,37 @@
 class Wallet
-  DENOMINATIONS = {:penny => 1, :nickel => 5, :dime => 10, :quarter => 25, :dollar => 100}
+  DENOMINATIONS = {
+                    :penny   => 1,
+                    :nickel  => 5,
+                    :dime    => 10,
+                    :quarter => 25,
+                    :dollar  => 100
+                  }
 
   def initialize
-    @wallet = {}
-    @total = 0
+    @wallet = Hash.new(0)
   end
 
   def <<(coin)
-    @wallet.has_key?(coin) ? @wallet[coin] += 1 :  @wallet[coin] = 1
+    @wallet[coin] += 1
   end
 
   def cents
-    new_wallet = @wallet.inject({}) do |h, (k,v)| 
-      h[k] = v * DENOMINATIONS[k] if DENOMINATIONS.has_key?(k)
-      h
+    new_wallet = Hash.new(0)
+    @wallet.each do |k, v|
+      new_wallet[k] = v * DENOMINATIONS[k] if DENOMINATIONS.has_key?(k)
     end
-    @total = new_wallet.values.inject(0, :+) # initialize at zero removed need to return @total if @wallet.empty?
+    total(new_wallet)
   end
 
   def take(*coins)
     coins.each do |coin|
       @wallet.has_key?(coin) ? @wallet[coin] -= 1 :  nil
     end
+  end
+
+  private
+
+  def total(wallet)
+    wallet.values.reduce(0, :+)
   end
 end
